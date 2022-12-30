82b43486de772003d9f482b79d8352bfcd072472
Quanosek <kubaklalo@gmail.com>
2022-12-30 22:39:11 +0100

    init

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pl">
+      <Head>
+        {/* meta */}
+        <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="theme-color" content="#000000" />
+
+        {/* apple */}
+        <meta name="mobile-wep-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="white" />
+        <meta name="apple-mobile-web-app-title" content="Fonetyka" />
+
+        {/* favicon */}
+        <link rel="icon" href="/logo/favicon.ico" sizes="any" />
+        <link rel="icon" href="/logo/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/logo/apple-180x180.png" />
+
+        {/* manifest */}
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

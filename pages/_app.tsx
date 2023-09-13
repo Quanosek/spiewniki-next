985a530207bd37ca3897c8ef021a823c291835c0
Krist0f0l0s <kb.olszewski@outlook.com>
2023-09-14 00:03:58 +0200

    cleaner getItem

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,15 +13,10 @@ export default function App({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     // set global color theme
-    const theme = localStorage.getItem("colorTheme")
-      ? localStorage.getItem("colorTheme")
-      : unlocked
-      ? "black"
-      : "light";
+    const theme = localStorage.getItem("colorTheme") || unlocked ? "black" : "light";
 
-    document.documentElement.className = `${
-      unlocked ? "accent_blue" : "accent_orange"
-    } ${theme}`;
+    document.documentElement.className = `${unlocked ? "accent_blue" : "accent_orange"
+      } ${theme}`;
 
     // prevent screen from sleeping
     let wakeLock: WakeLockSentinel | null = null;

5ace5ef17b4d4cb44ea116676e514f01d4b2aaba
Krist0f0l0s <kb.olszewski@outlook.com>
2023-11-12 23:59:23 +0100

    removed sorting from random button

--- scripts/buttons.ts
+++ scripts/buttons.ts
@@ -40,9 +40,9 @@ export function randomHymn(book: string | undefined) {
         let hymns = new Array();
 
         Collector.map(({ data }) => hymns.push(...data));
-        hymns = hymns.sort((a, b) =>
-          a.name.localeCompare(b.name, undefined, { numeric: true })
-        );
+        // hymns = hymns.sort((a, b) =>
+        //   a.name.localeCompare(b.name, undefined, { numeric: true })
+        // );
 
         const random = Math.floor(Math.random() * (hymns.length + 1));
 

c99dd195c7fbdf2d5df6021d4cf3c453e9c7694e
Jakub Kłało <kubaklalo@gmail.com>
2025-09-27 01:51:14 +0200

    fix type errors

---
+++ types/modules.d.ts
@@ -0,0 +1,21 @@
+declare module '*.css' {
+  const content: string
+  export default content
+}
+
+declare module '*.scss' {
+  const content: string
+  export default content
+}
+
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module 'the-new-css-reset/css/reset.css'
